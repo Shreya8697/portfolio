fb5fad3b1ab391e011c7399fe3932f546b20684a
Shreya Gupta <shreyaguptagit@gmail.com>
2025-02-03 21:41:36 +0530

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -2,10 +2,10 @@ export const contactsData = {
     email: 'shreyagupta8697@gmail.com',
     phone: '+91 9220908697',
     address: 'Nallasopara , Mumbai ,Maharashtra ',
-    github: 'https://github.com/shreya583',
+    github: 'https://github.com/Shreya8697',
     facebook: 'https://www.facebook.com/',
     linkedIn: 'https://www.linkedin.com/in/shreya-gupta8697/',
     twitter: 'https://twitter.com/Shreyag09068957?s=09',
     stackOverflow: 'https://stackoverflow.com/users/27711954/shreya-office',
-    devUsername: "shreya583"
+    devUsername: "Shreya8697"
 }
