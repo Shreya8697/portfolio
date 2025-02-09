4b8e3717ca9b84ee1940f6513072e9244369fe09
Shreya Gupta <shreyaguptagit@gmail.com>
2025-02-09 18:22:52 +0530

    Update personal-data.js

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -5,7 +5,7 @@ export const personalData = {
   description: "My name is Shreya Gupta. Self-taught, self-driven, passionate learner and developer focusing on Angular, React, Vue, Node, and Java Technologies. Looking forward to working with ingenious teams on challenging projects. I’m an easy going person that works well with everyone. I enjoy being around different types of people and I like to always challenge myself to improve at everything I do.I have a great passion for learning languages and exploring the world and also programming. I have always loved technology and programming. And this is my profession now.",
   email: 'shreyagupta8697@gmail.com',
   phone: '+91 9220908697',
-    address: 'Nalasopara , Mumbai ,Maharashtra ',
+    address: 'Nallasopara , Mumbai ,Maharashtra ',
     github: 'https://github.com/Shreya8697',
     facebook: 'https://www.facebook.com/',
     linkedIn: 'https://www.linkedin.com/in/shreya-gupta8697/',
@@ -13,5 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/',
   leetcode: "https://leetcode.com/",
   devUsername: "shreya",
-  resume: https://drive.google.com/file/d/1VpWrE3M2DeZYbbZF8tDLHGmSkdqJqxe3/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1VpWrE3M2DeZYbbZF8tDLHGmSkdqJqxe3/view?usp=sharing"
 }
